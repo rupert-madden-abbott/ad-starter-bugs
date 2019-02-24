6f39c7713105328b16d29c30681641166de8697a
Rupert Madden-Abbott <rupert@madden-abbott.com>
2019-02-24 22:05:18 +0000

    Add basic web app.

---
+++ src/main/java/com/github/rupert654/adstarterbugs/Application.java
@@ -0,0 +1,11 @@
+package com.github.rupert654.adstarterbugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+  public static void main(final String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
